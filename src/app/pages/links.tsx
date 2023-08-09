1059c366898edf34015cef50a33f324a48941cd2
minhle <duyminh2032000@gmail.com>
2023-08-09 09:20:54 +0700

    second commit

--- src/app/pages/links.tsx
+++ src/app/pages/links.tsx
@@ -10,31 +10,19 @@ export const links = [
   {
     index: 0,
     title: "Find me on Github",
-    href: "https://github.com/noworneverev",
+    href: "https://github.com/mjmpro01",
     icon: <FaGithub />,
   },
   {
     index: 1,
     title: "Find me on LinkedIn",
-    href: "https://www.linkedin.com/in/yan-ying-liao/",
+    href: "https://www.linkedin.com/in/minh-l%C3%AA-37a39a21a/",
     icon: <FaLinkedin />,
   },
   {
     index: 2,
     title: "Contact me via email",
-    href: "mailto:n9102125@gmail.com",
+    href: "duyminh2032000@gmail.com",
     icon: <FaEnvelope />,
-  },
-  {
-    index: 3,
-    title: "Find me on Medium",
-    href: "https://medium.com/@noworneverev",
-    icon: <FaMedium />,
-  },
-  {
-    index: 4,
-    title: "Find me on Blog",
-    href: "https://noworneverev.github.io/blog/",
-    icon: <FaBlog />,
-  },
+  }
 ];
